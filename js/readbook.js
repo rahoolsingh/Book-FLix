72060e192a51ff134346b7466c5e278493873997
Rahul Kumar Singh <rahulksingh3907@gmail.com>
2023-03-07 05:20:27 +0530

    fixes bugs

--- js/readbook.js
+++ js/readbook.js
@@ -4,104 +4,104 @@ var Array = queryString.split("?");
 var queryArray = Array[0].split("=");
 
 const fetchBookPreview = (bookId) => {
-    const bookPreviewUrl = `https://books.google.com/books?id=${bookId}&newbks=0&lpg=PP1&pg=PP1&output=embed`;
-    const bookPreviewIframe = document.createElement("iframe");
-    bookPreviewIframe.src = bookPreviewUrl;
-    bookPreviewIframe.classList.add("book-read-iframe");
+  const bookPreviewUrl = `https://books.google.com/books?id=${bookId}&newbks=0&lpg=PP1&pg=PP1&output=embed`;
+  const bookPreviewIframe = document.createElement("iframe");
+  bookPreviewIframe.src = bookPreviewUrl;
+  bookPreviewIframe.classList.add("book-read-iframe");
 
-    const bookPreviewContainer = document.querySelector(".main");
-    bookPreviewContainer.innerHTML = ""; // clear any previous preview
-    bookPreviewContainer.appendChild(bookPreviewIframe);
-  };
+  const bookPreviewContainer = document.querySelector(".main");
+  bookPreviewContainer.innerHTML = ""; // clear any previous preview
+  bookPreviewContainer.appendChild(bookPreviewIframe);
+};
 
 const fetchBooks = (query, box) => {
   fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=40`)
-    .then(response => response.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
       const books = data.items;
       const bookContainer = document.querySelector(box);
       console.log(data);
-      
-      books.forEach(book => {
-          const bookDiv = document.createElement("div");
-          bookDiv.classList.add("book");
-          
-          const bookImageDiv = document.createElement("div");
-          bookImageDiv.classList.add("book-image");
 
-          const bookImageLink = document.createElement("a");
+      books.forEach((book) => {
+        const bookDiv = document.createElement("div");
+        bookDiv.classList.add("book");
 
-          const bookImage = document.createElement("img");
-          const bookInnerDiv = document.createElement("div");
-          const bookTitle = document.createElement("h3");
-          bookTitle.id = "book-title-low";
-          const authorName = document.createElement("h5");
-          authorName.id = "author";
-          const available = document.createElement("h5");
-          available.className = "available";
-          const bookInner = document.createElement("p");
-          
+        const bookImageDiv = document.createElement("div");
+        bookImageDiv.classList.add("book-image");
 
+        const bookImageLink = document.createElement("a");
+
+        const bookImage = document.createElement("img");
+        const bookInnerDiv = document.createElement("div");
+        const bookTitle = document.createElement("h3");
+        bookTitle.id = "book-title-low";
+        const authorName = document.createElement("h5");
+        authorName.id = "author";
+        const available = document.createElement("h5");
+        available.className = "available";
+        const bookInner = document.createElement("p");
+
+        try {
           try {
-              try {
-                  bookImage.src = book.volumeInfo.imageLinks.thumbnail;
-              } catch (error) {
-                  bookImage.src = 'css\\images\\book-cover.png';
-              }
-              bookImageLink.href = '/read?'+book.id;
-              bookImage.alt = book.volumeInfo.title;
-              bookTitle.textContent = book.volumeInfo.title;
-              authorName.textContent = book.volumeInfo.authors[0];
-              var availablity = book.accessInfo.accessViewStatus;
+            bookImage.src = book.volumeInfo.imageLinks.thumbnail;
           } catch (error) {
-              bookImageLink.href = '?nan';
-              bookImage.alt = 'default';
-              bookTitle.textContent = 'Untitled eBook';
-              authorName.textContent = 'NaN';
+            bookImage.src = "css\\images\\book-cover.png";
           }
-          if(availablity == 'SAMPLE'){
-              available.className = 'available-sample'
-              available.textContent = 'Sample Only';
+          bookImageLink.href = "/read?" + book.id;
+          bookImage.alt = book.volumeInfo.title;
+          bookTitle.textContent = book.volumeInfo.title;
+          authorName.textContent = book.volumeInfo.authors[0];
+          var availablity = book.accessInfo.accessViewStatus;
+        } catch (error) {
+          bookImageLink.href = "?nan";
+          bookImage.alt = "default";
+          bookTitle.textContent = "Untitled eBook";
+          authorName.textContent = "NaN";
+        }
+        if (availablity == "SAMPLE") {
+          available.className = "available-sample";
+          available.textContent = "Sample Only";
+        } else if (availablity == "FULL_PUBLIC_DOMAIN") {
+          available.className = "available-full";
+          available.textContent = "Available";
+        } else {
+          available.className = "available-not";
+          available.textContent = "Not Available";
+        }
+        bookInnerDiv.appendChild(available);
+        bookImageLink.appendChild(bookImage);
+        bookImageDiv.appendChild(bookImageLink);
+        bookDiv.appendChild(bookImageDiv);
+        bookInnerDiv.appendChild(bookTitle);
+        bookInnerDiv.appendChild(authorName);
+        bookInnerDiv.appendChild(bookInner);
+        bookDiv.appendChild(bookInnerDiv);
 
-          }
-          else if(availablity == 'FULL_PUBLIC_DOMAIN'){
-              available.className = 'available-full'
-              available.textContent = 'Available';
-
-          }
-          else{
-              available.className = 'available-not'
-              available.textContent = 'Not Available';
-
-          }
-          bookInnerDiv.appendChild(available);
-          bookImageLink.appendChild(bookImage);
-          bookImageDiv.appendChild(bookImageLink);
-          bookDiv.appendChild(bookImageDiv);
-          bookInnerDiv.appendChild(bookTitle);
-          bookInnerDiv.appendChild(authorName);
-          bookInnerDiv.appendChild(bookInner);
-          bookDiv.appendChild(bookInnerDiv);
-          
-          bookContainer.appendChild(bookDiv);
+        bookContainer.appendChild(bookDiv);
       });
     })
-    .catch(error => {
-      console.log(error)
+    .catch((error) => {
+      console.log(error);
       const bookContainer = document.querySelector(box);
       const bookDiv = document.createElement("div");
       bookDiv.classList.add("error");
-      bookDiv.textContent= "Error: Something went wrong";
+      bookDiv.textContent = "Error: Something went wrong";
       bookContainer.appendChild(bookDiv);
-  });
+    });
 };
-if(queryArray[0] == 'bookflixsearch')
-{
-    fetchBooks(queryArray[1], '.box');
-    document.querySelector('.searchquery').textContent = "'"+queryArray[1]+"'";
+if (queryArray[0] == "bookflixsearch") {
+  queryArray[1] = queryArray[1].replace("+", " ");
+  fetchBooks(queryArray[1], ".box");
+  document.querySelector(".searchquery").textContent =
+    "'" + queryArray[1] + "'";
+  document.title = "Bookflix | " + queryArray[1];
+} else {
+  fetch(`https://www.googleapis.com/books/v1/volumes/${queryArray[0]}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const title = data.volumeInfo.title;
+      document.title = "Bookflix | " + title;
+    })
+    .catch((error) => console.error(error));
+  fetchBookPreview(queryArray[0]);
 }
-else{
-    fetchBookPreview(queryArray[0]);
-}
-
-
